3e3b78729eb6a4cd5646928f8f9d582a8e01cc89
Xiang Liu <xiang.liu@rightcapital.com>
2018-09-15 12:27:40 +0800

    Init project

---
+++ src/LaravelSentry.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Lead\LaravelSentry;
+
+
+class LaravelSentry
+{
+
+}
---
+++ src/LaravelSentryServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Lead\LaravelSentry;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSentryServiceProvider extends ServiceProvider
+{
+
+}
