a7ddb029f07cfe87591f117493ceb0f5f57b922b
Xiang Liu <xiang.liu@rightcapital.com>
2018-09-15 14:12:31 +0800

    Add processors

--- src/Processors/HttpRequestPayloadProcessor.php
+++ src/Processors/HttpRequestPayloadProcessor.php
@@ -1,10 +1,42 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Leap\LaravelSentry\Processors;
 
+use Raven_Processor;
+use Raven_Serializer;
 
-class HttpRequestPayloadProcessor
+/**
+ * 1) Report PUT payload
+ * 2) Report POST payload when Content-Type is not 'application/x-www-form-urlencoded'
+ * 3) Decode JSON payload
+ */
+class HttpRequestPayloadProcessor extends Raven_Processor
 {
+    public function process(&$data): void
+    {
+        if (isset($data['request']['method']) && in_array($data['request']['method'], ['PUT', 'POST'], true) && !isset($data['request']['data'])) {
+            // Sanitizer is called before processors, so here we need to sanitize manually
+            $serializer = new Raven_Serializer();
 
+            if (!empty($_POST)) {
+                $payload = $_POST;
+            } else { // POST payload will be in php://input, when Content-Type is not 'application/x-www-form-urlencoded'
+                $payload = file_get_contents('php://input');
+            }
+
+            if ($payload) {
+                if (app('request')->isJson()) {
+                    $json = json_decode($payload, true);
+
+                    if (JSON_ERROR_NONE === json_last_error()) {
+                        $payload = $json;
+                    }
+                }
+
+                $data['request']['data'] = $serializer->serialize($payload);
+            }
+        }
+    }
 }
--- src/Processors/SanitizeCookiesProcessor.php
+++ src/Processors/SanitizeCookiesProcessor.php
@@ -1,10 +1,51 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Leap\LaravelSentry\Processors;
 
+use Raven_Client;
+use Raven_Processor;
 
-class SanitizeCookiesProcessor
+/**
+ * @see \Raven_Processor_SanitizeHttpHeadersProcessor
+ * @see \Raven_Processor_RemoveCookiesProcessor
+ */
+class SanitizeCookiesProcessor extends Raven_Processor
 {
+    /**
+     * @var string[] The list of HTTP cookies to sanitize
+     */
+    private $patterns = [];
 
+    public function __construct(Raven_Client $client)
+    {
+        parent::__construct($client);
+    }
+
+    public function setProcessorOptions(array $options): void
+    {
+        $this->patterns = $options['patterns'] ?? [];
+
+        if (is_string(config('session.cookie'))) {
+            $this->patterns[] = config('session.cookie'); /** @see \Raven_Processor_SanitizeDataProcessor::sanitizeHttp() */
+        }
+    }
+
+    public function process(&$data): void
+    {
+        if (isset($data['request']['cookies']) && is_array($data['request']['cookies'])) {
+            foreach ($data['request']['cookies'] as $cookie => &$value) {
+                foreach ($this->patterns as $pattern) {
+                    if (is_string($pattern) && strlen($pattern)) {
+                        if (substr($pattern, 0, 1) === '/' && preg_match($pattern, $cookie)) {
+                            $value = self::STRING_MASK;
+                        } elseif (substr($pattern, 0, 1) !== '/' && strcasecmp($pattern, $cookie) === 0) {
+                            $value = self::STRING_MASK;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
